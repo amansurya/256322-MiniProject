6b66287292ce94ce65d58e417e9bc18d3ae95328
amansurya <40491650+amansurya@users.noreply.github.com>
2021-04-16 15:46:34 +0530

    Update calculator_operations.h

--- 3_Implementation/inc/calculator_operations.h
+++ 3_Implementation/inc/calculator_operations.h
@@ -1,1 +1,18 @@
 
+/**
+ * @file atm.h
+ * @author Mangu Aman Surya
+ * @brief Header file for ATM opertions assosiated functions
+ */
+
+#ifndef __ATM_H__
+#define __ATM_H__
+
+
+
+float pinchange();
+/**
+     * @brief function that is called to change the Personal Identification Number for a perticular account tow hich it is currently logged into. It has no parameters and no return values.
+     **/
+
+#endif  /* __ATM_H__  */
